2933744b4039ee0d01a084f7dcb36a7dd619ebb9
limjunzhao <165933606+limjunzhao@users.noreply.github.com>
2024-04-27 00:07:16 +0800

    maps

---
+++ Data/tsx/ob_house_03.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="ob_house_03" tilewidth="16" tileheight="16" tilecount="25" columns="5">
+ <image source="../../sprites sheet for maps/Terrains/buildings/ob_house_03.png" width="80" height="80"/>
+</tileset>
