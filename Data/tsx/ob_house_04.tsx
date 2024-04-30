08c657eb8fd6f2bb4bf9659aabe5ae5b9120380b
cheun <naacheun05@gmail.com>
2024-04-30 22:32:46 +0800

    clean

--- Data/tsx/ob_house_04.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="ob_house_04" tilewidth="16" tileheight="16" tilecount="20" columns="4">
- <image source="../../sprites sheet for maps/Terrains/buildings/ob_house_04.png" width="64" height="80"/>
-</tileset>
