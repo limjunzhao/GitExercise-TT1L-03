3aa69690b729a2a38c3d934d11bacddb349e5202
limjunzhao <165933606+limjunzhao@users.noreply.github.com>
2024-05-05 21:31:53 +0800

    maps import

--- latest update code/camera.py
+++ latest update code/camera.py
@@ -1,61 +1,59 @@
 import pygame 
 from player import Player 
-
+from support import *
 
 class CameraGroup (pygame.sprite.Group):
   def __init__ (self): 
     super().__init__()
     self.display_surface = pygame.display.get_surface()
 
-
     #camera offset
     self.half_w =  self.display_surface.get_size()[0] //2
     self.half_h =  self.display_surface.get_size()[1] //2
     self.offset = pygame.math.Vector2()
 
+    #creating the floor 
+    self.floor_surf = pygame.image.load('../Data/tmx/ground.png')
+    self.floor_rect = self.floor_surf.get_rect (topleft = (0,0))
 
-    # self.ground_surf = pygame.image.load('../Data/tmx/maps draft.png').convert_alpha ()
-    # self.ground_rect = self.ground_surf.get_rect(topleft = (0,0))
+    #zoom 
+    self.zoom_scale = 2
+    self.internal_surf_size = (1000, 1000)
+    self.internal_surf = pygame.Surface (self.internal_surf_size, pygame.SRCALPHA)
+    self.internal_rect = self.internal_surf.get_rect (center = (self.half_w, self.half_h))
+    self.internal_surface_size_vector = pygame.math.Vector2(self.internal_surf_size)
+    self.internal_offset = pygame.math.Vector2()
+    self.internal_offset.x = self.internal_surf_size[0] //2 - self.half_w
+    self.internal_offset.y = self.internal_surf_size[1] //2 - self.half_h
 
   def center_target_camera(self, player): 
     self.offset.x = player.rect.centerx - self.half_w
     self.offset.y = player.rect.centery - self.half_h
 
-
-    #zoom 
-    # self.zoom_scale = 1
-    # self.internal_surf_size = (1200, 1200)
-    # self.internal_surf = pygame.Surface (self.internal_surf_size, pygame.SRCALPHA)
-    # self.internal_rect = self.internal_surf.get_rect (center = (self.half_w, self.half_h))
-    # self.internal_surface_size_vector = pygame.math.Vector2(self.internal_surf_size)
-    # self.internal_offset = pygame.math.Vector2()
-    # self.internal_offset.x = self.internal_surf_size[0] //2 - self.half_w
-    # self.internal_offset.y = self.internal_surf_size[1] //2 - self.half_h
-  
   def zoom_keyboard_control(self):
     keys = pygame.key.get_pressed()
     if keys[pygame.K_q]:
-      self.zoom_scale += 0.01
+      self.zoom_scale += 0.05
     if keys[pygame.K_e]:
-      self.zoom_scale -= 0.01
-
+      self.zoom_scale -= 0.05
 
   def custom_draw(self, player): 
 
     self.center_target_camera(player)
-    #self.zoom_keyboard_control()
+    self.zoom_keyboard_control()
     
-    #self.internal_surf.fill('white')
+    self.internal_surf.fill('black')
 
-    #ground 
-    # self.ground_offset = self.ground_rect.topleft - self.offset 
-    # self.display_surface.blit (self.ground_surf,self.ground_rect)
 
-    for sprite in self.sprites(): 
-      offset_pos = sprite.rect.topleft - self.offset #+ self.internal_offset
-      self.display_surface.blit (sprite.image, offset_pos)
+    #drawing floor 
+    floor_offset_pos = self.floor_rect.topleft - self.offset + self.internal_offset
+    self.internal_surf.blit (self.floor_surf, floor_offset_pos)
 
-    # scaled_surf = pygame.transform.scale(self.internal_surf, self.internal_surface_size_vector * self.zoom_scale)
-    # scaled_rect = scaled_surf.get_rect(center = (self.half_w,self.half_h))
+    for sprite in sorted(self.sprites(), key = lambda sprite: sprite.rect.centery): 
+      offset_pos = sprite.rect.topleft - self.offset + self.internal_offset
+      self.internal_surf.blit (sprite.image, offset_pos)
+
+    scaled_surf = pygame.transform.smoothscale(self.internal_surf, self.internal_surface_size_vector * self.zoom_scale)
+    scaled_rect = scaled_surf.get_rect(center = (self.half_w, self.half_h))
   
-    # self.display_surface.blit (scaled_surf, scaled_rect)
+    self.display_surface.blit (scaled_surf, scaled_rect)
--- latest update code/level.py
+++ latest update code/level.py
@@ -3,6 +3,7 @@ from settings import *
 from tile import Tile
 from player import Player
 from camera import CameraGroup
+from support import *
 
 class Level:
 	def __init__(self):
@@ -18,19 +19,32 @@ class Level:
 		self.create_map()
 
 	def create_map(self):
-		#this is to check the row and helps to coordinates the pos.y
-		for row_index,row in enumerate(WORLD_MAP):
-			#check each of the column elements (x,p or empty) and helps to coordinate pos.x
-			for col_index, col in enumerate(row):
-				#multiply the col and row with the size of our tile so that it can fit
-				x = col_index * TILESIZE
-				y = row_index * TILESIZE
-				if col == 'x':
-					#check the pos of column and row and group it under the visible_sprites
-					#since the rock will have collision with the player, so we also put it under obstacle_sprites
-					Tile((x,y),[self.visible_sprites, self.obstacle_sprites])
-				if col == 'p':
-					self.player = Player((x,y),[self.visible_sprites], self.obstacle_sprites)
+		layout = {
+						'boundary': import_csv_layout('../Data/maps csv/ground_Floorblocks.csv'),
+
+
+		}
+
+		#style is boundary and layout is the csv file we import 
+		for style,layout in layout.items():
+			#this is to check the row and helps to coordinates the pos.y
+			for row_index,row in enumerate(layout):
+				#check each of the column elements (x,p or empty) and helps to coordinate pos.x
+				for col_index, col in enumerate(row):
+					#multiply the col and row with the size of our tile so that it can fit
+					if col != '-1':
+						x = col_index * TILESIZE
+						y = row_index * TILESIZE
+						if style == 'boundary':
+							Tile((x,y),[self.obstacle_sprites], 'invisible ')
+
+		# 		if col == 'x':
+		# 			#check the pos of column and row and group it under the visible_sprites
+		# 			#since the rock will have collision with the player, so we also put it under obstacle_sprites
+		# 			Tile((x,y),[self.visible_sprites, self.obstacle_sprites])
+		# 		if col == 'p':
+		# 			self.player = Player((x,y),[self.visible_sprites], self.obstacle_sprites)
+			self.player = Player((500,420),[self.visible_sprites], self.obstacle_sprites)
 				
 
 	def run(self):
