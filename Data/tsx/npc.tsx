aacba57da13d5efbc25b6feeb3725ebe05f841b3
limjunzhao <1221108298@student.mmu.edu.my>
2024-06-09 20:56:26 +0800

    maps

--- Data/tsx/npc.tsx
+++ Data/tsx/npc.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="npc" tilewidth="48" tileheight="48" tilecount="6" columns="0">
+<tileset version="1.10" tiledversion="1.10.2" name="npc" tilewidth="48" tileheight="48" tilecount="7" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="48" height="48" source="../../sprites sheet for maps/sprites/characters/players sprites/tile000.png"/>
@@ -18,5 +18,8 @@
  </tile>
  <tile id="5">
   <image width="48" height="48" source="../../sprites sheet for maps/sprites/characters/players sprites/tile049.png"/>
+ </tile>
+ <tile id="6">
+  <image width="48" height="48" source="../../sprites sheet for maps/sprites/characters/players sprites/tile055.png"/>
  </tile>
 </tileset>
